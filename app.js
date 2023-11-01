b08ab81dd28e65944b27fe3b9227967c4a8dc793
valrotkier <valrotkier@gmail.com>
2023-11-01 20:07:10 -0300

    Creando repo

---
+++ app.js
@@ -0,0 +1,7 @@
+//Sirve para chequear que se conecto mi archivo JS
+console.log("Conectado");
+
+let textoUsuario = prompt("Escribe un texto");
+let textoUsuario2 = prompt("Ahora otro");
+let userObject = textoUsuario + " " + textoUsuario2;
+alert(userObject);
