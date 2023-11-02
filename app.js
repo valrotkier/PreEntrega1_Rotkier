8ddc30511b234e4ad3d09b942b987d098c605a7c
valrotkier <valrotkier@gmail.com>
2023-11-02 19:53:39 -0300

    agregando algunos prompts con validaciones

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 //Sirve para chequear que se conecto mi archivo JS
 console.log("Conectado");
 
-let textoUsuario = prompt("Escribe un texto");
-let textoUsuario2 = prompt("Ahora otro");
-let userObject = textoUsuario + " " + textoUsuario2;
-alert(userObject);
+//Para que el usuario ingrese su nombre, parte de la base que mientras el nombre sea falso (que el usuario no haya escrito nada), te va a tirar alerta y luego vuelve a darte el prompt.
+let nombreUsuario;
+
+while (!nombreUsuario) {
+  nombreUsuario = prompt("Escribe tu nombre:").trim();
+  if (!nombreUsuario) {
+    alert("Por favor, escribe tu nombre.");
+  }
+}
+//Para que el usuario ingrese su apellido, parte de la base que mientras el input sea falso (que el usuario no haya escrito nada), te va a tirar alerta y luego vuelve a darte el prompt.
+let apellidoUsuario;
+
+while (!apellidoUsuario) {
+  apellidoUsuario = prompt("Escribe tu apellido:").trim();
+  if (!apellidoUsuario) {
+    alert("Por favor, escribe tu apellido.");
+  }
+}
+let bienvenidoUsuario =
+  "Ahora tienes acceso a la página " +
+  nombreUsuario +
+  " " +
+  apellidoUsuario +
+  "!";
+alert(bienvenidoUsuario);
+
+//Para que el usuario ingrese un numer, parte de la base que mientras el nombre sea falso (que el usuario no haya escrito nada), te va a tirar alerta y luego vuelve a darte el prompt.
+let userName;
+
+do {
+  userName = prompt("Elige un nombre de usuario");
+} while (!userName);
+
+alert("Tu usuario es: " + userName);
