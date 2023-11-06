6e6d8f4b6c42226bfe02608cf4f2de37c7aa6613
valrotkier <valrotkier@gmail.com>
2023-11-06 17:13:35 -0300

    agrego bucle al implementar "el mejor de 3"

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ jugarJuego();
 function eligeMovimientoComputadora() {
   const randomNumber = Math.random();
   let eleccionComputadora = "";
-  // genero una eleccion al azar para la computadora
+  // Aca se genera una eleccion al azar para la computadora
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
     eleccionComputadora = "Piedra";
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
@@ -18,50 +18,77 @@ function eligeMovimientoComputadora() {
 }
 
 function jugarJuego() {
-  const inputUsuario = prompt("Elige Piedra, Papel o Tijera:");
-  //Aca declaramos una variable para la seleccion de usuario que tenga un formato mas uniforme y valida, pasamos lo que escribio ('string') a 'Piedra' 'Papel' o 'Tijera' especificamente.
-  const eleccionUsuario =
-    inputUsuario.trim().charAt(0).toUpperCase() +
-    inputUsuario.slice(1).toLowerCase();
+  let usuarioGana = 0;
+  let computadoraGana = 0;
 
-  if (
-    eleccionUsuario === "Piedra" ||
-    eleccionUsuario === "Papel" ||
-    eleccionUsuario === "Tijera"
-  ) {
-    const eleccionComputadora = eligeMovimientoComputadora();
-
-    let resultado = "";
-    if (eleccionUsuario === "Tijera") {
-      if (eleccionComputadora === "Piedra") {
-        resultado = "PERDISTE";
-      } else if (eleccionComputadora === "Papel") {
-        resultado = "GANASTE";
-      } else {
-        resultado = "EMPATE";
-      }
-    } else if (eleccionUsuario === "Papel") {
-      if (eleccionComputadora === "Piedra") {
-        resultado = "GANASTE";
-      } else if (eleccionComputadora === "Papel") {
-        resultado = "EMPATE";
-      } else {
-        resultado = "PERDISTE";
-      }
-    } else if (eleccionUsuario === "Piedra") {
-      if (eleccionComputadora === "Piedra") {
-        resultado = "Empate";
-      } else if (eleccionComputadora === "Papel") {
-        resultado = "PERDISTE";
-      } else {
-        resultado = "GANASTE";
-      }
+  while (usuarioGana < 2 && computadoraGana < 2) {
+    const inputUsuario = prompt(
+      "Juega al mejor de 3. Elige Piedra, Papel o Tijera:"
+    );
+    if (inputUsuario === null) {
+      break; // El usuario canceló el juego
     }
 
-    alert(
-      `${resultado}. Elegiste ${eleccionUsuario}. Computadora eligió ${eleccionComputadora}.`
-    );
+    //Aca declaramos una variable para la seleccion de usuario que tenga un formato mas uniforme y valida, pasamos lo que escribio ('string') a 'Piedra' 'Papel' o 'Tijera' especificamente.
+    const eleccionUsuario =
+      inputUsuario.trim().charAt(0).toUpperCase() +
+      inputUsuario.slice(1).toLowerCase();
+
+    // Comprobación de elección válida del jugador
+    if (
+      eleccionUsuario === "Piedra" ||
+      eleccionUsuario === "Papel" ||
+      eleccionUsuario === "Tijera"
+    ) {
+      const eleccionComputadora = eligeMovimientoComputadora();
+
+      let resultado = "";
+      // Esto determina el resultado de la ronda
+      if (eleccionUsuario === "Tijera") {
+        if (eleccionComputadora === "Piedra") {
+          resultado = "PERDISTE";
+          computadoraGana++;
+        } else if (eleccionComputadora === "Papel") {
+          resultado = "GANASTE";
+          usuarioGana++;
+        } else {
+          resultado = "EMPATE";
+        }
+      } else if (eleccionUsuario === "Papel") {
+        if (eleccionComputadora === "Piedra") {
+          resultado = "GANASTE";
+          usuarioGana++;
+        } else if (eleccionComputadora === "Papel") {
+          resultado = "EMPATE";
+        } else {
+          resultado = "PERDISTE";
+          computadoraGana++;
+        }
+      } else if (eleccionUsuario === "Piedra") {
+        if (eleccionComputadora === "Piedra") {
+          resultado = "Empate";
+        } else if (eleccionComputadora === "Papel") {
+          resultado = "PERDISTE";
+          computadoraGana++;
+        } else {
+          resultado = "GANASTE";
+          usuarioGana++;
+        }
+      }
+
+      alert(
+        `${resultado}. Elegiste ${eleccionUsuario}. Computadora eligió ${eleccionComputadora}.`
+      );
+    } else {
+      alert(
+        "Lo que escribiste no es aceptable para poder jugar. Por favor, elige entre Piedra, Papel o Tijera."
+      );
+    }
+  }
+
+  if (usuarioGana >= 2) {
+    alert("Ganaste el mejor de 3!");
   } else {
-    alert("Por favor, elige Piedra, Papel o Tijera.");
+    alert("La computadora te gano el mejor de 3.");
   }
 }
